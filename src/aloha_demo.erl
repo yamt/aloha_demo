8b6524bac9173d07c85fd3abf522c67d06b8df7b
YAMAMOTO Takashi <yamt@mwd.biglobe.ne.jp>
2013-09-03 01:58:41 +0900

    adapt to aloha changes

--- src/aloha_demo.erl
+++ src/aloha_demo.erl
@@ -31,7 +31,7 @@ start() ->
 
     ets:new(aloha_datapath, [set, named_table, public]),
 
-    aloha_tcp:init_tables(),
+    aloha_tables:init_tables(),
     aloha_neighbor:start_link(),
     aloha_upper:start({aloha_demo, 9999}, [{recv_mode, async}]),
 %    aloha_upper:start({aloha_demo, 9999}, [{mod, gen_tcp}]),
