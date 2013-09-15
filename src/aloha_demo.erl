7f69dff9030f06b677b83293c5cbe8f08f13f092
YAMAMOTO Takashi <yamt@mwd.biglobe.ne.jp>
2013-09-15 23:49:03 +0900

    follow aloha api change

--- src/aloha_demo.erl
+++ src/aloha_demo.erl
@@ -31,8 +31,7 @@ start() ->
 
     ets:new(aloha_datapath, [set, named_table, public]),
 
-    aloha_tables:init_tables(),
-    aloha_neighbor:start_link(),
+    aloha:start(),
     aloha_discard:start({aloha_demo, 9}, [{recv_mode, async}]),
     aloha_upper:start({aloha_demo, 9999}, [{recv_mode, async}]),
 %    aloha_upper:start({aloha_demo, 9999}, [{mod, gen_tcp}]),
